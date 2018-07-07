fc0bbc3119bb8b5985ed6fc7dea5e63482542d51
Ahmed Abdelghafar <a.abdelghafar@glc.local>
2018-07-07 10:11:16 +0200

    frist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+
+
+const appRoute:Routes = [
+  {path:'',component:AppComponent},
+  {path:'login',component:LoginComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute),
+ReactiveFormsModule,
+FormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
